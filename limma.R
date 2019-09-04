0d9bc8a528f17b135f30208790f32f2429e3a799
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-09-04 11:20:47 +0200

    Update limma.R

--- limma.R
+++ limma.R
@@ -18,27 +18,27 @@ Install_Multiples_Packages(c('edgeR', 'limma'))
 
 
 
-folder = "folder_containing_gene_counts_reads" 
-annof <- "annnoatation_from_biomart.txt"
+folder <- "folder_conteining_genes_counts" #Change it to your folder
+annof <- "annotation_file_from_biomart.txt" #change it to your biomart file
 
 samples <- read.table("samples_all.txt",header=T, as.is=T)
 countf <- file.path(folder, paste0(samples$SAMPLE_ID, '_ReadsPerGene.counts'))
 
 
-counts = readDGE(countf)$counts
+counts <- readDGE(countf)$counts
 class(counts)
 dim(counts)
 colnames(counts)
 # 2. Filter weakly expressed and noninformative (e.g., non-aligned) features:
-noint = rownames(counts) %in% c("N_multimapping","N_noFeature","N_ambiguous")
-cpms = cpm(counts)
+noint <- rownames(counts) %in% c("N_multimapping","N_noFeature","N_ambiguous")
+cpms <- cpm(counts)
 
 mean(colSums(counts[!noint,])/colSums(counts))
 
 
 # In edgeR, it is recommended to remove features without at least 1 read per million in n of the samples, where n is the size of the smallest group of replicates, 
-keep = rowSums(cpms >1) >=3 & !noint
-counts = counts[keep,]
+keep <- rowSums(cpms >1) >=3 & !noint
+counts <- counts[keep,]
 class(counts)
 
 dge <- DGEList(counts=counts)
